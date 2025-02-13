a5617e78a6e4541a0dd630713d3028def88e8d27
alfianekrhmn <alfian.eka481@gmail.com>
2025-02-13 14:45:28 +0700

    Mengerjakan Search Algo dengan big o notation

--- logic_nolep/searchAlgo.js
+++ logic_nolep/searchAlgo.js
@@ -1,5 +1,26 @@
 const searchRotatedArray = (nums, target) => {
-    
+    let start = 0
+    let end = nums.length - 1
+    while(start <= end){
+        let middle = Math.floor((start+end)/2)
+        if(nums[middle] === target){
+            return middle
+        }
+        if(nums[start] <= nums[middle]){
+            if(nums[start]<=target&&target<nums[middle]){
+                end = middle-1
+            }else{
+                start = middle+1
+            }
+        }else{
+            if(nums[middle]< target && target<=nums[end]){
+                start=middle+1
+            }else{
+                end = middle-1
+            }
+        }
+    }
+    return -1
 };
 
 // Test Case
