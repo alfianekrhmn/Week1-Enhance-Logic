237ff8ae413d4b3fabb3d72bd2c07557d1c67eb9
alfianekrhmn <alfian.eka481@gmail.com>
2025-03-09 21:31:05 +0700

    mengerjakan oop bagian class bank

--- logic_nolep/oop.js
+++ logic_nolep/oop.js
@@ -1,27 +1,118 @@
+class Bank {
+  constructor(bankName){
+    this.bankName = bankName
+  }
+
+  register(name, type, amount){
+    let accountNumber = Math.floor(Math.random() * 10000000000)
+    if(amount < 20000){
+      return `Saldo awal kurang dari minimum saldo yang ditentukan`
+    }else{
+      return `Selamat datang ke ${this.bankName}, ${name}. Nomor Akun anda adalah ${accountNumber}. Total saldo adalah ${amount}`
+    }
+  }
+}
+
 class Person {
-  constructor(name, age) {
-    this.name = name
-    this.age = age
-  }
-
-  introduce() {
-    console.log(`Hello... my name's ${this.name} and i'm ${this.age} years old`)
+  constructor(nameCustomer){
+    this.nameCustomer = nameCustomer
   }
 }
 
-const person1 = new Person(`Mike`, 30)
-person1.introduce()
-
-
-class Animal {
-  constructor(name) {
-    this.name = name
-  }
-
-  makeSound() {
-    console.log(`${this.name} makes a sound`)
-  }
+class Member {
+  // Tulis Code Disini
 }
 
-const animal = new Animal('Dog')
-animal.makeSound()
+class Platinum extends Member{
+  // Tulis Code Disini
+}
+
+class Silver extends Member{
+  // Tulis Code Disini
+}
+
+class Transaction {
+  // Tulis Code Disini
+}
+
+// TESTCASE
+// TIDAK BOLEH MENGUBAH CODE DI BAWAH INI
+
+let yudhistiraBank = new Bank('Yudhistira Bank')
+let nadia = new Person('Nadia')
+
+yudhistiraBank.register(nadia, 'platinum', 5000)
+// Saldo awal kurang dari minimum saldo yang ditentukan
+yudhistiraBank.register(nadia, 'platinum', 54000)
+//Selamat datang ke Yudhistira Bank, Nadia. Nomor Akun anda adalah 6332937. Total saldo adalah 54000
+let nadiaAccount = nadia.bankAccount
+
+/* PASTIKAN BAHWA SALDO SELALU BERKURANG ATAU BERTAMBAH UNTUK SETIAP TRANSAKSI */
+nadiaAccount.credit(300000)
+// Anda sukses menyimpan uang ke dalam bank.
+
+nadiaAccount.credit(1000)
+// Belum memenuhi minimal uang yang dapat di setor
+
+nadiaAccount.debet(200000, 'Beli Keyboard')
+// Anda sukses menarik uang dari bank
+
+nadiaAccount.debet(130000, 'Beli Keyboard Lagi')
+// Saldo minimum anda tidak terpenuhi untuk melakukan transaksi.
+nadiaAccount.debet(600000, 'Bisa gak ya lebih besar dari balance ? ')
+// Saldo anda tidak cukup
+
+let semmi = new Person('Semmi Verian')
+yudhistiraBank.register(semmi, 'silver', 10000000)
+let semmiAccount = semmi.bankAccount
+
+nadiaAccount.transfer(semmiAccount, 100000)
+// Anda sukses transfer ke Semmi Verian
+nadiaAccount.transfer(semmiAccount, 1000000)
+// Anda gagal transfer ke Semmi Verian
+
+console.log(semmiAccount)
+// Silver {
+//   memberName: 'Semmi Verian',
+//   accountNumber: 1319650,
+//   minimumBalance: 10000,
+//   balance: 10100000,
+//   transactions: [
+//     Transaction {
+//       nominal: 100000,
+//       status: 'credit',
+//       date: 2025-01-28T07:13:54.802Z,
+//       note: 'transfer dari akun Nadia'
+//     }
+//   ],
+//   type: 'silver'
+// }
+
+console.log(nadiaAccount)
+// Platinum {
+//   memberName: 'Nadia',
+//   accountNumber: 3971487,
+//   minimumBalance: 50000,
+//   balance: 54000,
+//   transactions: [
+//     Transaction {
+//       nominal: 300000,
+//       status: 'credit',
+//       date: 2025-01-28T07:13:54.800Z,
+//       note: 'nyetor'
+//     },
+//     Transaction {
+//       nominal: 200000,
+//       status: 'debet',
+//       date: 2025-01-28T07:13:54.801Z,
+//       note: 'Beli Keyboard'
+//     },
+//     Transaction {
+//       nominal: 100000,
+//       status: 'debet',
+//       date: 2025-01-28T07:13:54.802Z,
+//       note: 'transfer ke akun Semmi Verian'
+//     }
+//   ],
+//   type: 'platinum'
+// }
